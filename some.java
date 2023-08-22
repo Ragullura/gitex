82e5dfdb71134f8910fcb2af74be3a5be81de98f
Ragullura <ragullura.005@gmail.com>
2023-08-22 12:47:42 +0530

    change the a value to 50

--- some.java
+++ some.java
@@ -2,7 +2,7 @@ public class some {
     
     public static void main(String[] args) {
         
-        int a = 10;
+        int a = 50;
         int b = 20;
         System.out.println(a+b);
     }
